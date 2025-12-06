2558945ad7a6e4f4c2df63104ce08d1828b3b777
rodas7-art <rodaswores@gmail.com>
2025-12-07 01:21:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const[num1, setNum1]=useState("");
+  const[num2, setNum2]=useState("");
+  const [result, setResult]=useState("");
+  function addition(){
+setResult(Number(num1)+Number(num2));
+  }
+    function substraction(){
+setResult(Number(num1)-Number(num2));
+  }
+
+    function multiplication(){
+setResult(Number(num1)*Number(num2));
+  }
+
+    function division(){
+      if(Number(num2)===0){
+        setResult("undefined");
+      }
+      else{
+setResult(Number(num1)/Number(num2)); 
+      }
+  }
+  return (
+    <div className="calculator">
+     <h1>Hello Queen bee</h1>
+     <p>okay now we are going to be testing what we can do okay but it is going to be trial and error! okay first lets take in numbers and ddo the calculator</p>
+    
+    
+    <input 
+    type="number"
+    value={num1}
+    onChange={(e) =>  setNum1(e.target.value)}
+    className="input-box"
+    />
+<input 
+  type="number"
+  value={num2}
+  onChange={(e) => setNum2(e.target.value)}
+  className="input-box"
+  />
+<div className="buttons">
+<button onClick={addition}>+</button>
+<button onClick={substraction}>-</button>
+<button onClick={multiplication}>*</button>
+<button onClick={division}>/</button>
+</div>
+<h2>Result: {result}</h2>
+    </div>
+
+  )
+}
+
+export default App
